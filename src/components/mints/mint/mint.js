5e8210837e1209a0a6d32733b4ae0895e547dd4e
dragochar <zacyungblut@gmail.com>
2022-03-29 23:29:20 -0400

    only unused vars errors trying

--- src/components/mints/mint/mint.js
+++ src/components/mints/mint/mint.js
@@ -53,7 +53,7 @@ const Mint = ({ mint, setCurrentMintId }) => {
         //hi there
     };
 
-    useEffect(() => {
+    useEffect((mint) => {
         checkIfWalletIsConnected();
         setProgressNow(((mint.likes.length)/((mint.likes.length)+(mint.dislikes.length))*100));
 
