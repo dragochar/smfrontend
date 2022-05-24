5478e39d86de830af4f657f2d727bee665400183
dragochar <zacyungblut@gmail.com>
2022-05-24 17:37:01 -0400

    new wallet for tD

--- src/wallets/tdwallets.js
+++ src/wallets/tdwallets.js
@@ -2,6 +2,7 @@
 var AdminWallets = [
     'CeVXxgDm6MR5biBzRqLW7U9uv47nfrZMYJ5BtTbGLPY3',
     '4nLscmknYKfQ6iUqWKupGpeShdzNoATNKCNrH7SFjFQc',
+    'HTyoeUH6asHfzUgre66LaniJG6SpsxEbhmco2kiznLxS'
 ];
 
 
