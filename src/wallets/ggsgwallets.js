09aab35fc71d060b454e68428398e9f32868442b
dragochar <zacyungblut@gmail.com>
2022-05-26 10:12:06 -0400

    wallets

--- src/wallets/ggsgwallets.js
+++ src/wallets/ggsgwallets.js
@@ -2,6 +2,9 @@
 var AdminWallets = [
     'CeVXxgDm6MR5biBzRqLW7U9uv47nfrZMYJ5BtTbGLPY3',
     'DhoMkFE2gGqWVoJhVcgA25zEJxqNC9BZkn3pzF4Pd9ww',
+    'CT5wyvYnYSttSr1VqmTirzFDa1tbGuHTWkx7uxkGwfA6',
+    '3gfyx64vrWH4FPX291fM1fb5UPmHZLq8cxQeoMxjmFeG',
+    '3UjVc4Ratzn5CESVWcLHESxDmsVs5KeVtYECu9A2qLqv'
 ];
 
 
