e2ce92970b11ad1e1ede6ac2af53ee00d6c78a03
dragochar <zacyungblut@gmail.com>
2022-05-26 12:49:48 -0400

    new wallet for ggsg

--- src/wallets/ggsgwallets.js
+++ src/wallets/ggsgwallets.js
@@ -5,7 +5,8 @@ var AdminWallets = [
     'CT5wyvYnYSttSr1VqmTirzFDa1tbGuHTWkx7uxkGwfA6',
     '3gfyx64vrWH4FPX291fM1fb5UPmHZLq8cxQeoMxjmFeG',
     '3UjVc4Ratzn5CESVWcLHESxDmsVs5KeVtYECu9A2qLqv',
-    '8spWe4dZsxt17yWxE5zoY6uEWrj6JUzHuShvkgAUeYVk'
+    '8spWe4dZsxt17yWxE5zoY6uEWrj6JUzHuShvkgAUeYVk',
+    '2hVK5rSi9X3yeZ1WM9ioUuhG2A9tphzaoKwqKB34TyQH'
 ];
 
 
